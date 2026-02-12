b19be08523af21b63532aafead5fa5cc8171841d
Kevin Suarez <kevin.fontecha@quasfar.com.co>
2026-02-12 15:41:26 -0500

    config: actualizar configuracion de Reflex y dependencias
    - rxconfig.py:
      * Cambiar app_name de 'asl_recognition' a 'asl_app'
      * Deshabilitar plugin de sitema innecesario
    
    - requirements.txt:
      * Consolidar dependencias (TensorFlow, Reflex, FastAPI, OpenCV, etc.)
    
    - .gitignore:
      * Actualizar con patrones relevantes para proyecto

--- rxconfig.py
+++ rxconfig.py
@@ -3,9 +3,10 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="asl_recognition",
+    app_name="asl_app",
     env=rx.Env.DEV,
     frontend_port=3000,
     backend_port=8000,
     tailwind={},
+    disable_plugins=["reflex.plugins.sitemap.SitemapPlugin"],
 )
